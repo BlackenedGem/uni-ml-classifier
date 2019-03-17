e0e7e7ef287e188484868bac74874d63743fea55
blackenedgem <Chrisroberts97@btinternet.com>
2019-03-17 16:20:08 +0000

    46.64%

--- coursework_classifier.py
+++ coursework_classifier.py
@@ -107,9 +107,9 @@ class MyNetwork(nn.Module):
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, dilation=2)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, stride=1, padding=1, dilation=2)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1)
         self.bn3 = nn.BatchNorm2d(128)
 
         self.lin1 = nn.Linear(in_features=128 * 2 * 2, out_features=400)

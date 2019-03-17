84a9484427f4c16aaf543128da843af0860f3cc4
blackenedgem <Chrisroberts97@btinternet.com>
2019-03-17 19:07:39 +0000

    Improve kernels. Got to 53.46%

--- coursework_classifier.py
+++ coursework_classifier.py
@@ -108,12 +108,12 @@ class MyNetwork(nn.Module):
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, stride=1, padding=1, dilation=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=4, stride=1, padding=2, dilation=2)
         self.bn2 = nn.BatchNorm2d(128)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=4, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
 
-        self.lin1 = nn.Linear(in_features=128 * 2 * 2, out_features=400)
+        self.lin1 = nn.Linear(in_features=128 * 3 * 3, out_features=400)
         self.lin2 = nn.Linear(in_features=400, out_features=200)
         self.lin3 = nn.Linear(in_features=200, out_features=100)
 
